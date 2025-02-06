8d7fa882f6bca3688bb53a78e8ed677fd5693e24
Noureddine Missoum <missoum.noureddine18@g.ens-kouba.dz>
2025-02-06 22:42:32 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,9 +4,9 @@ package com.mycompany.projet_de_poo;
 public class Main {
     public static void main(String[] args) {
 
-        Etudiant etudiant1 = new Etudiant("Ahmed", "Ali", 10, "Homme", "E12345", 10);
-        Etudiant_ENSK etudiant2 = new Etudiant_ENSK("Sara", "Mohamed", 19, "Femme", "E67890", 15.0);
-        Etudiant_ENSK etudiant3 = new Etudiant_ENSK("missoum", "noureddine", 17, "Homme", "E67891",17);
+        Etudiant etudiant1 = new Etudiant("oussama", "hocini", 10, "Homme", "M12345", 10);
+        Etudiant_ENSK etudiant2 = new Etudiant_ENSK("amina", "bnkafil", 19, "Femme", "M67890", 15.0);
+        Etudiant_ENSK etudiant3 = new Etudiant_ENSK("missoum", "noureddine", 17, "Homme", "M67891",17);
 
 
         Diplome diplome = new Diplome();
